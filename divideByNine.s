01435fd672cd6190211da5cfd0a03534ce16cd93
Stevie <outsidetheglass93@gmail.com>
2019-11-14 16:27:14 -0800

    Delete divideByNine.s

--- divideByNine.s
+++
@@ -1,7 +0,0 @@
-.text
-.global _start
-_start:
-		LDR 	R6, =#65
-		LSR		R10, R6, #3 @ This line divides by nine.
-		SUB 	R11, R10, #1 @ is incorrect by a bit at higher values, this is correct if above is wrong.
-.END
